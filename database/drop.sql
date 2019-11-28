03e62e88d2c3b45f0efbf9e341b7dd7e2a079feb
ksarson <37591778+ksarson@users.noreply.github.com>
2019-11-28 12:33:22 -0500

    Add files via upload

---
+++ database/drop.sql
@@ -0,0 +1,7 @@
+drop table student cascade constraints;
+drop table professor cascade constraints;
+drop table section_records cascade constraints;
+drop table room cascade constraints;
+drop table department cascade constraints;
+drop table course cascade constraints;
+drop table student_records cascade constraints;
