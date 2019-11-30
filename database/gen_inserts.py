fa3579201441ceb7318ac5e1645e1febf9fc2bbb
majid171 <majid.joseph7@gmail.com>
2019-11-30 15:49:04 -0500

    finished insert script

--- database/gen_inserts.py
+++ database/gen_inserts.py
@@ -54,17 +54,68 @@ names = ['Jacki Mcelyea',
 'Emogene Morain',
 'Mildred Bulli']
 
+file_name = ''
 semesters = ['S', 'W', 'F']
 student_numbers = []
 dep_names = ['Computer Science', 'Chemistry', 'Business', 'Engineering']
 dep_id = ['CS', 'CH', 'BU', 'EN']
 prof_id = [9001, 9002, 9003, 9004, 9005, 9006]
 prof_names = ['Bobby Jean', 'Claire Baker', 'Harold Beaumont', 'Katie Smith', 'Brandon Brown', 'William Davis']
-section_numbers = [1234, 1235, 1236, 1237, 1238, 1239, 1240, 1241, 1242, 1243, 1244, 1245]
+section_numbers = []
 room_ids = [101, 102, 103, 201, 202, 203] 
 week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 times = ['8:30am-9:50am', '10:00am-11:20am', '11:30am-12:50pm', '1:00pm-2:20pm', '2:30pm-3:50pm', '4:00pm-5:20pm', '5:30pm-6:50pm', '7:00pm-9:50pm']
 course_names = ['COMP1050', 'COMP2080', 'COMP2260', 'CHEM3470', 'CHEM3801', 'CHEM4540', 'BUSS4540', 'BUSS2500', 'BUSS1000', 'ENGI4540', 'ENGI3540', 'ENGI1000']
+
+room_insert = [
+	"INSERT INTO ROOM VALUES (101, 'ERIE HALL', 110);",
+	"INSERT INTO ROOM VALUES (102, 'ERIE HALL', 60);",
+	"INSERT INTO ROOM VALUES (103, 'ERIE HALL', 35);",
+	"INSERT INTO ROOM VALUES (201, 'ESSEX HALL', 105);",
+	"INSERT INTO ROOM VALUES (202, 'ESSEX HALL', 55);",
+	"INSERT INTO ROOM VALUES (203, 'ESSEX HALL', 30);",
+]
+
+department_insert = [
+	"INSERT INTO DEPARTMENT VALUES ('CS', 'Computer Science');",
+	"INSERT INTO DEPARTMENT VALUES ('CH', 'Chemistry');",
+	"INSERT INTO DEPARTMENT VALUES ('BU', 'Business');",
+	"INSERT INTO DEPARTMENT VALUES ('EN', 'Engineering');"
+]
+
+professor_insert = [
+	"INSERT INTO PROFESSOR VALUES (9001, 'BOBBY', 'JEAN', 'BJEAN@UWINDSOR.CA', 'CS');",
+	"INSERT INTO PROFESSOR VALUES (9002, 'CLAIRE', 'BAKER', 'CBAKER@UWINDSOR.CA', 'CS');",
+	"INSERT INTO PROFESSOR VALUES (9003, 'HAROLD', 'BEAUMONT', 'HBEAUMONT@UWINDSOR.CA', 'CH');",
+	"INSERT INTO PROFESSOR VALUES (9004, 'KATIE', 'SMITH', 'KSMITH@UWINDSOR.CA', 'CH');",
+	"INSERT INTO PROFESSOR VALUES (9005, 'BRANDON', 'BROWN', 'BBROWN@UWINDSOR.CA', 'BU');",
+	"INSERT INTO PROFESSOR VALUES (9006, 'WILLIAM', 'DAVIS', 'WDAVIS@UWINDSOR.CA', 'EN');"
+]
+
+course_insert = [
+	"INSERT INTO COURSE VALUES ('COMP1050', 'INTRO TO PROGRAMMING', 'DESCRIPTION OF COMP1050', 'CS');",
+	"INSERT INTO COURSE VALUES ('COMP2080', 'OOP USING JAVA', 'DESCRIPTION OF COMP2080', 'CS');",
+	"INSERT INTO COURSE VALUES ('COMP2260', 'DATABASE MANAGEMENT SYSTEMS', 'DESCRIPTION OF COMP2260', 'CS');",
+	"INSERT INTO COURSE VALUES ('CHEM3470', 'SPECTROSCOPIC STRUCTURE IDENTIFICATION', 'DESCRIPTION OF CHEM3470', 'CH');",
+	"INSERT INTO COURSE VALUES ('CHEM3801', 'PRINCIPLES OF INSTRUMENTAL ANALYSIS', 'DESCRIPTION OF CHEM3810', 'CH');",
+	"INSERT INTO COURSE VALUES ('CHEM4540', 'BIO AND SUSTAINABLE MATERIALS', 'DESCRIPTION OF CHEM4540', 'CH');",
+	"INSERT INTO COURSE VALUES ('BUSS4540', 'BUSINESS MANAGEMENT', 'DESCRIPTION OF BUSS4540', 'BU');",
+	"INSERT INTO COURSE VALUES ('BUSS2500', 'ECONOMICS I', 'DESCRIPTION OF BUSS2500', 'BU');",
+	"INSERT INTO COURSE VALUES ('BUSS1000', 'ECONOMICS II', 'DESCRIPTION OF BUSS1000', 'BU');",
+	"INSERT INTO COURSE VALUES ('ENGI4540', 'MECHANICS I', 'DESCRIPTION OF ENGI4540', 'EN');",
+	"INSERT INTO COURSE VALUES ('ENGI3540', 'MECHANICS II', 'DESCRIPTION OF ENGI3540', 'EN');",
+	"INSERT INTO COURSE VALUES ('ENGI1000', 'ENGINEERING PRINCIPLES', 'DESCRIPTION OF ENGI1000', 'EN');"
+]
+
+def insert_other_data(data):
+	f = open(file_name, 'a')
+
+	for line in data:
+		f.write(line + '\n')
+
+	f.write('\n')
+	f.close()
+	return
 
 def rand_num(n):
 	range_start = 10**(n-1)
@@ -78,8 +129,8 @@ def gen_gpa():
 	return round(random.uniform(50.0 , 99.0), 2)
 
 def gen_students():
-	file_name = sys.argv[1]
-	f = open(file_name, 'w')
+	# file_name = sys.argv[1]
+	f = open(file_name, 'a')
 
 	output = ''
 
@@ -98,17 +149,23 @@ def gen_students():
 		output += '\'' + dep_id[index] + '\');\n'
 		f.write(output)
 	
+	f.write('\n')
 	f.close()
 	return
 
 def gen_section_records():
-	file_name = sys.argv[3]
-	f = open(file_name, 'w')
+	# file_name = sys.argv[3]
+	f = open(file_name, 'a')
 
 	output = ''
-	for i in range(0, len(section_numbers)):
+	for i in range(0, len(course_names)):
 		output = 'insert into section_records values(01,'
-		sec = section_numbers[rand_num_range(0, len(section_numbers) - 1)]
+		
+		sec = rand_num(4)
+		# while sec in section_numbers:
+		# 	sec = rand_num(4)
+		section_numbers.append(sec)
+
 		output += '\'' + str(sec) + '\','
 		output += '\'' + course_names[rand_num_range(0, len(course_names) - 1)] +'\','
 		output += str(prof_id[rand_num_range(0, len(prof_id) - 1)]) + ','
@@ -119,20 +176,26 @@ def gen_section_records():
 		output += '\'' + time_slot[0] + '\',' + '\'' + time_slot[1] + '\');\n'
 		# print(output)
 		f.write(output)
-
+	f.write('\n')
 	f.close()
 
 def gen_student_records():
 	
-	file_name = sys.argv[2]
-	f = open(file_name, 'w')
+	# file_name = sys.argv[2]
+	f = open(file_name, 'a')
 
 	output = ''
-
+	temp_list = []
 	for student in range(0, len(names)):
 		for i in range(0, 3):
 			output = 'insert into student_records values('
-			output += str(rand_num(4)) + ','
+
+			temp = rand_num(4)
+			while temp in temp_list:
+				temp = rand_num(4)
+				temp_list.add(temp)
+
+			output += str(temp) + ','
 			output += str(student_numbers[student]) + ','
 			grade = rand_num(2)
 			section_rec = section_numbers[rand_num_range(0, len(section_numbers) - 1)]
@@ -142,18 +205,26 @@ def gen_student_records():
 			output += '\'2017\','
 			output += '\'' + semesters[rand_num_range(0, len(semesters) - 1)] + '\');\n'
 			f.write(output)
-
+	f.write('\n')
 	f.close()
 	return
 
 def check_args():
-	if len(sys.argv) != 4:
+	if len(sys.argv) != 2:
 		print('ERROR: Please run as...')
-		print('\tpython3 gen_students.py <student insert file> <student records insert file> <section records insert file>')
+		print('\tpython3 gen_inserts.py <insert file>')
 	else:
+		global file_name
+		file_name = sys.argv[1]
+		f = open(file_name, 'w')
+		f.close()
+		insert_other_data(room_insert)
+		insert_other_data(department_insert)
 		gen_students()
-		gen_student_records()
+		insert_other_data(professor_insert)
+		insert_other_data(course_insert)
 		gen_section_records()
+		gen_student_records()
 
 def main():
 	check_args()
