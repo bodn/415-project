72f1d0ed6e4aa45dc0612a7089f63f3744c2a8ab
Steven Bodnar <stevenbodn@gmail.com>
2019-12-01 17:49:24 -0500

    Added section for student courses

--- web-app/src/app/student/student-details/student-details.component.ts
+++ web-app/src/app/student/student-details/student-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StudentService } from 'src/app/service/student.service';
+import { BehaviorSubject } from 'rxjs';
+import { Student } from 'src/app/models/student';
 
 @Component({
   selector: 'app-student-details',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentDetailsComponent implements OnInit {
   panelOpenState = false;
-  constructor() { }
+  loading$ = new BehaviorSubject(false);
+  studentId: number;
+  studentDetails = null;
+  courseDetails$ = new BehaviorSubject<any>([]);
+
+  constructor(private route: ActivatedRoute, private studentService: StudentService) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.loading$.next(true);
+      this.studentId = params.id;
+      this.studentService.getStudentDetails(+params.id).subscribe(resp => {
+        this.studentDetails = resp.data[0];
+        this.loading$.next(false);
+      });
+
+      this.studentService.getStudentCourses(+params.id).subscribe(resp => {
+        this.courseDetails$.next(resp.data);
+      });
+    });
   }
 
 }
