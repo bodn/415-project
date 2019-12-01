2d1fd7385305efb6a40c9352f4420a7e35f31638
Steven Bodnar <stevenbodn@gmail.com>
2019-12-01 18:23:40 -0500

    Added service for course related operations

---
+++ web-app/src/app/service/course.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  private url;
+  constructor(private http: HttpClient) {
+    this.url = environment.apiURL;
+  }
+
+  getAllCourses(): Observable<any> {
+    return this.http.get(`${this.url}/courses/`);
+  }
+
+}
