ed547d32cd4b0846fe795a5458afebcd2d55046c
Steven Bodnar <stevenbodn@gmail.com>
2019-12-01 22:45:12 -0500

    push to remote

--- web-app/src/app/home-dashboard/home/home.component.ts
+++ web-app/src/app/home-dashboard/home/home.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { BackendService } from 'src/app/service/backend.service';
-import { BehaviorSubject } from 'rxjs';
-import { CourseService } from 'src/app/service/course.service';
+import { Component, OnInit } from "@angular/core";
+import { BackendService } from "src/app/service/backend.service";
+import { BehaviorSubject, from } from "rxjs";
+import { CourseService } from "src/app/service/course.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   loading$ = new BehaviorSubject(false);
   studentData$ = new BehaviorSubject<any>([]);
   courseData$ = new BehaviorSubject<any>([]);
   currentTabIndex = 0;
-  constructor(private backendService: BackendService, private courseService: CourseService) { }
+  constructor(
+    private backendService: BackendService,
+    private courseService: CourseService,
+  ) {}
   ngOnInit() {
-    this.loading$.next(true);
+    if (window.history.state.tab) {
+      this.currentTabIndex = window.history.state.tab;
+    }
     this.backendService.getStudents().subscribe(res => {
       this.loading$.next(false);
       this.studentData$.next(res.data);
@@ -24,11 +30,9 @@ export class HomeComponent implements OnInit {
     this.courseService.getAllCourses().subscribe(res => {
       this.courseData$.next(res.data);
     });
-
   }
 
   showTab(index) {
     this.currentTabIndex = index;
   }
-
 }
